a6bcd87c280842f399f48363d6c2828ff0f62bb8
RelayConnectionsAdmin <122250892+RelayConnectionsAdmin@users.noreply.github.com>
2023-01-26 21:11:46 -0600

    Update UserRequests.js

--- src/api/UserRequests.js
+++ src/api/UserRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://backend-4g2lexwz2-relayconnectionsadmin.vercel.app/" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
